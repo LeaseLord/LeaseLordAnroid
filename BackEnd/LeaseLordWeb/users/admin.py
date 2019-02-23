9d34dfeedd5ab927f647ba57d7d8fbd0b500d6e9
Leah Bissey <leahbissey@gmail.com>
2019-02-23 16:47:31 -0500

    modified database models

--- BackEnd/LeaseLordWeb/users/admin.py
+++ BackEnd/LeaseLordWeb/users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import LandLord
+from .models import Tenant
+
 admin.site.register(LandLord)
+admin.site.register(Tenant)
 # Register your models here.
--- BackEnd/LeaseLordWeb/users/models.py
+++ BackEnd/LeaseLordWeb/users/models.py
@@ -8,9 +8,13 @@ class LandLord(AbstractUser):
         phone = models.CharField(max_length=50)
         email = models.CharField(max_length=100)
         vemail = models.CharField(max_length =100)
-        name = models.CharField(max_length=100)
         organization = models.CharField(max_length=100)
 
+class Tenant(models.Model):
+        landlord = models.ForeignKey(LandLord, on_delete=models.DO_NOTHING)
+        leasestart = models.DateField()
+        leaseend = models.DateField()
+        paymentdue = models.DateField()
 
 
 # Create your models here.
