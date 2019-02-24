28f30a61d848f2f0e2e869fe810698f551726b96
Hussnain Zaidi <husnainzaidi99@gmail.com>
2019-02-24 16:21:09 -0500

    Models changed: The user models were changed to fit the needs of our application. A tenant model was added that extends the base user model. A property manager model was added that extends the base user model. Previously we had a tenant model but there was no way to authenticate a tenant during login. Now both a property manager and a tenant can be logged in using a registered username and password.

--- BackEnd/LeaseLordWeb/users/admin.py
+++ BackEnd/LeaseLordWeb/users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import LandLord
+from .models import PropertyManager
 from .models import Tenant
+from .models import User
 
-admin.site.register(LandLord)
+admin.site.register(User)
+admin.site.register(PropertyManager)
 admin.site.register(Tenant)
 # Register your models here.
--- BackEnd/LeaseLordWeb/users/models.py
+++ BackEnd/LeaseLordWeb/users/models.py
@@ -4,17 +4,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class LandLord(AbstractUser):
+class User(AbstractUser):
         phone = models.CharField(max_length=50)
         email = models.CharField(max_length=100)
         vemail = models.CharField(max_length =100)
-        organization = models.CharField(max_length=100)
+        is_tenant = models.BooleanField(default=False)
+        is_landlord = models.BooleanField(default=False)
+
+class PropertyManager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='pm_profile')
+    organization = models.CharField(max_length=100)
 
 class Tenant(models.Model):
-        landlord = models.ForeignKey(LandLord, on_delete=models.DO_NOTHING)
-        leasestart = models.DateField()
-        leaseend = models.DateField()
-        paymentdue = models.DateField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='tenant_profile')
+    propertymanager= models.ForeignKey(PropertyManager, on_delete=models.CASCADE)
+    leasestart = models.DateField()
+    leaseend = models.DateField()
+    paymentdue = models.DateField()
+
+
+
+
 
 
 # Create your models here.
