e3bf17c5389b13b1d0607d052e58a8bdda5dbadc
Hussnain Zaidi <husnainzaidi99@gmail.com>
2019-02-27 14:02:58 -0500

    settings.py and login.html updated:
    settings.py was updated to include the static directory and login.html 
    was updated to include security token and to fix form input names.
    
    Rest of the files are from running the app and making migrations.

--- BackEnd/LeaseLordWeb/LeaseLordWeb/settings.py
+++ BackEnd/LeaseLordWeb/LeaseLordWeb/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 LOGIN_REDIRECT_URL = '/admin'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
