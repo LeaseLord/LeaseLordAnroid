f1db51926c1460239d15cac46e8f1a8ac1b53cf5
Hussnain Zaidi <husnainzaidi99@gmail.com>
2019-04-27 11:11:48 -0400

    Hussnain Zaidi:
    Created Models for Document App.
    Added functions in views for viewing leases and uploading leases.
    Created the documents and viewdocuments html files with William Owenby. 
    Updated settings.py to include the documents app.

--- BackEnd/LeaseLordWeb/document/models.py
+++ BackEnd/LeaseLordWeb/document/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from users.models import Tenant, PropertyManager
 # Create your models here.
+
+class Lease(models.Model):
+    tenant = models.ForeignKey(Tenant,related_name="ten",on_delete=models.CASCADE)
+    pm = models.ForeignKey(PropertyManager,related_name="propm",on_delete=models.CASCADE)
+    lease = models.FileField(upload_to='leases/')
--- BackEnd/LeaseLordWeb/document/views.py
+++ BackEnd/LeaseLordWeb/document/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseRedirect, HttpResponse
+from users.models import Tenant, PropertyManager
+from django.template import loader
+from .models import Lease
 
 # Create your views here.
+def UploadLease(request):
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            if request.user.is_propertymanager:
+                pm1 = PropertyManager.objects.get(user=request.user)
+                id = request.POST.get('tenant',None)
+                tenant = Tenant.objects.get(id = id)
+                document = request.FILES['document']
+                lease = Lease(tenant=tenant,pm=pm1,lease=document)
+                lease.save()
+                html = "<script> if(!alert('File Uplaoded!')){window.location = window.location.pathname;} </script>"
+                content = loader.render_to_string('sharedoc/documents.html')
+                upper,lower = content.split('</body>',1)
+                upper += html
+                upper += lower
+                return HttpResponse(upper)
+    else:
+        if request.user.is_authenticated:
+            pm1 = PropertyManager.objects.get(user=request.user)
+            return render(request,'sharedoc/documents.html',{'pm':pm1})
+        else:
+            return HttpResponseRedirect('/login')
+
+def ViewLeases(request):
+    if request.user.is_authenticated:
+        if request.user.is_propertymanager:
+            pm1 = PropertyManager.objects.get(user=request.user)
+            return render(request,'sharedoc/viewdocuments.html',{'pm':pm1})
+        else:
+            tenant = Tenant.objects.get(user=request.user)
+            return render(request,'sharedoc/viewdocuments.html',{'tenant':tenant})
+    else:
+        return HttpResponseRedirect('/login')
